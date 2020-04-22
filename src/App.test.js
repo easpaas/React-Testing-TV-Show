ae403bf57deecfb33cd73632b0d12e6c5c868d57
evan <aspaasevan@gmail.com>
2020-04-21 17:40:01 -0700

    exported mockEpisodeData as episodeData for test files

--- src/App.test.js
+++ src/App.test.js
@@ -4,8 +4,13 @@ import userEvent from '@testing-library/user-event';
 import {fetchShow as mockFetchShow} from './api/fetchShow';
 import App from './App';
 
+import {data} from './episodeData';
+
 jest.mock('./api/fetchShow');
 
 test('renders without errors', () => {
+  mockFetchShow.mockResolvedValue(data);
+  // const {debug} = render(<App />);
+
 })
 
